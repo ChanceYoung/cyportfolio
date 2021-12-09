caf240094b6c2dd7bca418d5495305b8c88b8a60
root <root@DESKTOP-HVILAKK.localdomain>
2021-12-09 12:44:49 -0700

    added temporary console logging of message from ccontact form on the frontend

--- api/index.js
+++ api/index.js
@@ -56,30 +56,35 @@ app.get('/assoc', (req, res) => {
 })
 
 app.post('/api/email', (req, res) => {
-    const transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.NODEMAILER_EMAIL,
-            pass: process.env.NODEMAILER_PASS,
-        },
-    })
-    const mailOptions = {
-        from: process.env.NODEMAILER_EMAIL,
-        to: process.env.NODEMAILER_EMAIL,
-        subject: `from: ${req.body.sender_name}`,
-        text: req.body.message,
-    }
+    // const transport = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: process.env.NODEMAILER_EMAIL,
+    //         pass: process.env.NODEMAILER_PASS,
+    //     },
+    // })
+    // const mailOptions = {
+    //     from: process.env.NODEMAILER_EMAIL,
+    //     to: process.env.NODEMAILER_EMAIL,
+    //     subject: `from: ${req.body.sender_name}`,
+    //     text: req.body.message,
+    // }
+    console.log('***' + req.body.sender_name + '***\n')
+    console.log('Message:\n')
+    console.log(req.body.message)
 
-    transport.sendMail(mailOptions, (err, res) => {
-        if (err) {
-            console.log('failed to send email')
-        } else {
-            console.log('email sent')
-        }
-    })
+    // transport.sendMail(mailOptions, (err, res) => {
+    //     if (err) {
+    //         console.log('failed to send email')
+    //     } else {
+    //         console.log('email sent')
+    //     }
+    // })
     res.sendStatus(200)
 })
 
 app.listen(port, () => {
     console.log(`API is now listening at http://localhost:${port}`)
+    console.log('TEMPORARY MESSAGING LOGS\n')
+
 })
