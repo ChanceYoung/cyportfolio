93ecae8ab392cee789b0da541e04d7b7873ee005
Chance <chanceyoungwebdev@gmail.com>
2022-01-12 19:50:26 -0700

    changed the connection information for the pool in the api

--- api/index.js
+++ api/index.js
@@ -13,13 +13,7 @@ if (process.env.NODE_ENV == 'dev') {
     }
 }
 
-const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432,
-})
+const pool = new Pool()
 
 app.use(express.json())
 
