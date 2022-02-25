01fe0904b139c33e594514cecd58531f95c0fb4d
Chance <chanceyoungwebdev@gmail.com>
2022-02-25 08:10:07 -0700

    cleaned up login logic

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt')
 const dbservice = require('./services/dbservice')
 const { v4 } = require('uuid')
 const { app, startServer } = require('./configs/appConfig')
+var cookieExpiration = new Date()
+cookieExpiration.setTime(date.getTime() + 30 * 60 * 1000)
 
 app.use('/secure', async (req, res, next) => {
     const sessionID = req.cookies.session.sessionID
@@ -23,38 +25,41 @@ app.get('/posts', async (req, res) => {
 app.post('/login', async (req, res) => {
     const logininfo = req.body
     const dbresult = await dbservice.user.getUserPasswd(logininfo.username)
-    console.log(dbresult)
+    console.table(dbresult)
     console.log(dbresult === null)
     if (dbresult === null) {
+        console.log('403 sent')
         res.sendStatus(403)
-    }
-    const isVerified = await bcrypt.compare(
-        logininfo.password,
-        dbresult.hashed_password
-    )
-    console.log(isVerified)
-    if (isVerified) {
-        const sessionID = v4()
-        console.log(sessionID)
-        const sessionResult = await dbservice.session.addSessionInfo(
-            dbresult.user_id,
-            sessionID,
-            logininfo.username
-        )
-        res.cookie(
-            'session',
-            { sessionID, userid: dbresult.user_id },
-            {
-                httpOnly: true,
-                sameSite: 'strict',
-                expires: new Date.now() + 30 * 60 * 1000,
-            }
-        )
-        res.send(dbresult)
     } else {
-        res.sendStatus(403)
+        const isVerified = await bcrypt.compare(
+            logininfo.password,
+            dbresult.hashed_password
+        )
+        console.log(isVerified)
+        if (isVerified) {
+            const sessionID = v4()
+            console.log(sessionID)
+            const sessionResult = await dbservice.session.addSessionInfo(
+                dbresult.user_id,
+                sessionID,
+                logininfo.username
+            )
+            res.cookie(
+                'session',
+                { sessionID, userid: dbresult.user_id },
+                {
+                    httpOnly: true,
+                    sameSite: 'strict',
+                    expires: cookieExpiration,
+                }
+            )
+            res.send(dbresult)
+        } else {
+            res.sendStatus(403)
+        }
     }
 })
+
 app.get('/secure', async (req, res) => {
     const id = req.cookies.session.userid
     const result = await dbservice.user.getUserById(id)
