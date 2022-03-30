3085a1d96f65b925c466808f855930105434925a
Chance Young <Chanceyoungwebdev@gmai.com>
2022-03-29 21:22:55 -0600

    uncommentted getuserpasswd

--- api/services/userService.js
+++ api/services/userService.js
@@ -1,19 +1,19 @@
 const { pool } = require('../configs/poolConfig.js')
-// const getUserPasswd = async username => {
-//     try {
-//         const res = await pool.query(
-//             'Select user_id, hashed_password from portfolio.session_user where username = $1;',
-//             [username]
-//         )
-//         if (res.rowCount > 0) {
-//             return res.rows[0]
-//         } else {
-//             return null
-//         }
-//     } catch (err) {
-//         return err.stack
-//     }
-// }
+const getUserPasswd = async username => {
+    try {
+        const res = await pool.query(
+            'Select user_id, hashed_password from portfolio.session_user where username = $1;',
+            [username]
+        )
+        if (res.rowCount > 0) {
+            return res.rows[0]
+        } else {
+            return null
+        }
+    } catch (err) {
+        return err.stack
+    }
+}
 
 const getUserByName = async userid => {
     console.log('getting user by name')
