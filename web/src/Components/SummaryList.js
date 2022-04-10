d40d8522cd33a8ccef8767f06aecce2c6ce86f7a
Chance Young <Chanceyoungwebdev@gmai.com>
2022-04-09 23:44:30 -0600

    added in protections

--- web/src/Components/SummaryList.js
+++ web/src/Components/SummaryList.js
@@ -1,29 +1,36 @@
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 
 const SummaryList = ({ listofposts }) => {
     const navigate = useNavigate()
 
-    const onPostClick = (id) => {
-       navigate(`/projects/${id}`)
+    const onPostClick = id => {
+        navigate(`/projects/${id}`)
     }
+    console.log(listofposts)
     return (
         <>
-            {listofposts.map((post) => (
-                <div className="card w-50 col p-1" key={post.id}>
-                    <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <ReactMarkdown>{post.goal}</ReactMarkdown>
-                        <h6 className="card-text">Skills:{post.skills.map(skill => <p key={skill}>{skill}</p>)}</h6>
-                        <button
-                            onClick={() => onPostClick(post.id)}
-                            className="btn btn-secondary"
-                        >
-                            View
-                        </button>
+            {listofposts &&
+                listofposts.map(post => (
+                    <div className="card w-50 col p-1" key={post.id}>
+                        <div className="card-body">
+                            <h5 className="card-title">{post.title}</h5>
+                            <ReactMarkdown>{post.goal}</ReactMarkdown>
+                            <h6 className="card-text">
+                                Skills:
+                                {post.skills.map(skill => (
+                                    <p key={skill}>{skill}</p>
+                                ))}
+                            </h6>
+                            <button
+                                onClick={() => onPostClick(post.id)}
+                                className="btn btn-secondary"
+                            >
+                                View
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
         </>
     )
 }
