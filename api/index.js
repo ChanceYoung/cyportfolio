57bf9734b7f4d6933c60b3315161870e4d84a3f6
Chance Young <Chanceyoungwebdev@gmai.com>
2022-04-16 19:40:46 -0600

    fixed the api

--- api/index.js
+++ api/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const dbservice = require('./services/dbservice')
-const { app, startServer } = require('./configs/appConfig')
-const { auth } = require('express-oauth2-jwt-bearer')
-
-const checkJWT = auth({
-    audience: 'undefined',
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-})
-
-app.get('/', (req, res) => {
-    res.send('hit the cyportfolio api')
-})
-
-app.get('/posts', async (req, res) => {
-    const results = await dbservice.posts.getPosts()
-    res.send(JSON.stringify(results))
-})
-
-app.get('/profile', checkJWT, async (req, res) => {
-    console.table(req.params)
-    const result = await dbservice.user.getUserInfo(req.params.user)
-    if (result === null) res.sendStatus(500)
-    else res.send(result)
-})
-=======
 import startServer from './configs/appConfig.js'
->>>>>>> simplephotos
 
 startServer()
