c750403b0f334c651456ae23033e491136add122
Chance Young <Chanceyoungwebdev@gmai.com>
2022-04-29 00:28:04 -0600

    added in the annotations layer for my resume pdf

--- web/src/views/ResumeView.jsx
+++ web/src/views/ResumeView.jsx
@@ -1,28 +1,29 @@
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import ChanceYoungResume from '../Assets/ChanceYoungResume.pdf' 
-import { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import ChanceYoungResume from '../Assets/ChanceYoungResume.pdf'
+import { useState } from 'react'
 
 const ResumeView = () => {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-  
+    const [numPages, setNumPages] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
+
     function onDocumentLoadSuccess({ numPages }) {
-      setNumPages(numPages);
+        setNumPages(numPages)
     }
-  
+
     return (
-      <div className='container'>
-        <div className='d-flex justify-content-center bg-secondary rounded'>
-        <Document
-        className='pt-4'
-          file={ChanceYoungResume}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
+        <div className="container">
+            <div className="d-flex justify-content-center bg-secondary rounded">
+                <Document
+                    className="pt-4"
+                    file={ChanceYoungResume}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                >
+                    <Page pageNumber={pageNumber} />
+                </Document>
+            </div>
         </div>
-      </div>
-    );
+    )
 }
 
 export default ResumeView
